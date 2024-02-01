9d0fce4b236a323c3aabe13a4443b3c12113dd09
CirusDane <154769003+CirusDane@users.noreply.github.com>
2024-02-01 12:43:27 -0800

    Update bundle.js

--- dist/bundle.js
+++ dist/bundle.js
@@ -3503,7 +3503,7 @@ var Incremancer;
                     this.model.partsPCMod *= e,
                     this.model.zombieDamagePCMod *= e,
                     this.model.zombieHealthPCMod *= e,
-                    this.model.PlagueVatPCMod *= scalingFactor
+                    this.model.PlagueVatPC *= scalingFactor
             }
         }
         acceptOffer() {
