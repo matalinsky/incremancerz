89061a6daa78d781256fbc2a8530217c7f5ba79a
CirusDane <154769003+CirusDane@users.noreply.github.com>
2024-09-03 12:48:54 -0700

    Update bundle.js

--- dist/bundle.js
+++ dist/bundle.js
@@ -2102,7 +2102,7 @@ var Incremancer;
                 case this.types.AvionicsPC:
                     return "Harpy speed: " + n(this.gameModel.harpySpeed);
                 case this.types.SkeleMove:
-                    return "Skeleton speed: " + n(this.gameModel.moveSpeed);
+                    return "Skeleton speed: " + n(this.skeleton.moveSpeed);
                 case this.types.ShockPC:
                     return "Attack Speed multiplier: " + Math.round(100 * this.gameModel.ShockPCMod - 100) + "%";  
                 case this.types.prest_multPC:
